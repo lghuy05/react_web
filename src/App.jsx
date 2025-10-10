af67855bd4ab6915113c3d25cedb2fd22146442e
lghuy05 <luonggiahuy1909@gmail.com>
2025-10-09 23:06:24 -0400

    added api fetch from movie api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import heroImg from "./assets/hero.png"
 import Search from "./components/Search.jsx"
 
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+const API_BASE_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
+
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('I am batman');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}t=Titanic`;
+      const response = await fetch(endpoint);
+      const data = await response.json();
+      alert(JSON.stringify(data, null, 2));
+      console.log(API_KEY)
+    } catch (error) {
+      console.log(`Error fetching movies: ${error}`);
+      setErrorMessage("Error fetching movies. Please try again");
+    }
+  }
+  useEffect(() => {
+    fetchMovies();
+  }, []);
   return (
     <main>
       <div className="pattern" />
@@ -11,9 +30,12 @@ const App = () => {
         <header>
           <img src={heroImg} alt="Hero Banner" />
           <h1>Find <span className="text-gradient">movies</span> you'll enjoy without the hassle</h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
-
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className="all-movies">
+          <h2>All movies</h2>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
       </div>
 
     </main>
